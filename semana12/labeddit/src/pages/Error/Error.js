55d3834232e684c7890bf591c6c356993dc8c7a5
LuisMijias <luismijias@gmail.com>
2021-09-02 16:32:57 -0300

    criando rotas

---
+++ semana12/labeddit/src/pages/Error/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorPage = () => {
+    return(
+        <p>Página de Erro</p>
+    )
+}
+
+export default ErrorPage
---
+++ semana12/labeddit/src/pages/SignUpPage/SignUpPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUpPage = () => {
+    return (
+        <p>Página de Cadastro</p>
+    )
+}
+
+export default SignUpPage
